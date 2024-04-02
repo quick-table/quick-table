847c194629355bf70237ac191c9de178b3b86076
Komlan Kodoh <komlankodoh@gmail.com>
2024-04-02 15:15:19 -0600

    did minor adjustment and backend progress

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,12 +1,15 @@
 import { purgeCss } from 'vite-plugin-tailwind-purgecss';
 import { sveltekit } from '@sveltejs/kit/vite';
-import { defineConfig } from 'vite';
+import { defineConfig } from 'vitest/config';
 
 export default defineConfig({
 	plugins: [sveltekit(), purgeCss()],
+	test: {
+		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
 	server: {
 		fs: {
 			strict: false
 		}
-	}
+	},
 });

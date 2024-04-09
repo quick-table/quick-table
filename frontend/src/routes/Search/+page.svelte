3f1585a100a66a40e5eb375b0f1569d1bbba8b42
Dobson154 <happyLAW708@gmail.com>
2024-04-09 13:50:20 -0500

    Attempt to add dynamic search results and dynamic restaurant navigation

--- frontend/src/routes/Search/+page.svelte
+++ frontend/src/routes/Search/+page.svelte
@@ -1,5 +1,65 @@
 <script>
+	import { onMount } from 'svelte';
+	import { $location } from 'svelte-routing';
+	import { useQuery } from '@sveltestack/svelte-query';
+	import { useMutation } from '@sveltestack/svelte-query';
+
 	let results = ['Restaurant 1', 'Restaurant 2', 'Restaurant 3', 'Restaurant 4'];
+	const { data: searchResults, isLoading, error } = useQuery('allEntries', fetchAllEntries);
+	const [mutateR, { data: nameData, isLoading: isNameLoading, error: nameError }] = useMutation(rData => fetchRestaurantResults(rData));
+
+	onMount(() => {
+		const unsubscribe = $location.subscribe(location => {
+			const state = location.state;
+			if (state && state.results) {
+				results = state.results;
+			}
+			if (!state){
+				results = fetchAllEntries();
+			}
+		})
+		return unsubscribe;
+	});
+
+	async function fetchAllEntries() {
+    	try {
+      		const response = await fetch("/api/restaurants");
+      		const data = await response.json();
+			results = data;
+			return results;
+		}
+		catch (error) {
+      		console.error('Error fetching all entries:', error);
+      		throw new Error('Failed to fetch all entries');
+		}
+	}
+
+	async function fetchRestaurantResults(restaurantData) {
+		try {
+			const response = await fetch("/api/restaurants", {
+				method: 'POST',
+				headers: { 'Content-Type': 'application/json' },
+				body: JSON.stringify(restaurantData)
+			});
+
+			const rData = await response.json();
+
+			navigateToRestaurantPage(rData);
+		}
+		catch (error) {
+			console.error('Error fetching search results:', error);
+		}
+	}
+
+	function handleRestaurantSearch(event) {
+		event.preventDefault();
+		mutateName(rData);
+	}
+
+	function navigateToRestaurantPage(results) {
+		navigate('/Restaurant', { state: { results: results } });
+	}
+
 </script>
 
 <div class="min-h-screen bg-primary-300">
@@ -12,7 +72,7 @@
 		<nav class="list-nav max-w-fit">
 			<ul class="border-solid border-secondary-200">
 				{#each results as result}
-					<li
+					<li on:click={handleRestaurantSearch}
 						class="m-1 py-8 sm:px-24 md:px-32 lg:px-48 border-4 border-solid border-secondary-300 flex items-center hover:bg-primary-400"
 					>
 						<span
